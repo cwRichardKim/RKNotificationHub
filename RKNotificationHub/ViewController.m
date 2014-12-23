5e275cad875d95076cebe962edbbcc8a739e1d67
cwrichardkim93 <cwrichardkim93@gmail.com>
2014-12-23 12:08:15 +0800

    changed example

--- RKNotificationHub/ViewController.m
+++ RKNotificationHub/ViewController.m
@@ -22,32 +22,35 @@
     [self setupButton];
     
     UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"earth"]];
-    imageView.frame = CGRectMake(50, 50, 70, 70);
+    imageView.frame = CGRectMake(self.view.frame.size.width/2 - 35, 70, 70, 70);
     
     hub = [[RKNotificationHub alloc]initWithView:imageView];
-    [hub moveCircleByX:-5 Y:5];
+    [hub moveCircleByX:-5 Y:5]; // moves the circle five pixels left and 5 down
     [self.view addSubview:imageView];
-}
-
--(UIButton *)setupButton
-{
-    UIColor* color = [UIColor colorWithRed:.15 green:.67 blue:.88 alpha:1];
-    
-    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(50, 400, 200, 60)];
-    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [button setTitle:@"Messages" forState:UIControlStateNormal];
-    [button setBackgroundColor:color];
-    button.layer.cornerRadius = 5;
-    [button addTarget:self action:@selector(testIncrement) forControlEvents:UIControlEventTouchUpInside];
-
-    [self.view addSubview:button];
-    return button;
 }
 
 -(void)testIncrement
 {
     [hub increment];
+    
     [hub pop];
+//    [hub blink];
+//    [hub bump];
+}
+
+-(void)setupButton
+{
+    UIColor* color = [UIColor colorWithRed:.15 green:.67 blue:.88 alpha:1];
+    
+    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(50, 400, 200, 60)];
+    button.center = self.view.center;
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [button setTitle:@"Increment" forState:UIControlStateNormal];
+    [button setBackgroundColor:color];
+    button.layer.cornerRadius = 5;
+    [button addTarget:self action:@selector(testIncrement) forControlEvents:UIControlEventTouchUpInside];
+
+    [self.view addSubview:button];
 }
 
 - (void)didReceiveMemoryWarning {
