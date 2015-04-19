c41d9d140167ba019a356fff24ebba77a55d436f
cevitcejbo <cevitcejbo@gmail.com>
2015-04-19 03:26:10 +0300

    Add tests for changing count

--- RKNotificationHubTests/RKNotificationHubTests.m
+++ RKNotificationHubTests/RKNotificationHubTests.m
@@ -47,57 +47,94 @@ describe(@"setView:andCount:", ^{
     });
 });
 
-// changing the count
-
-describe(@"increment", ^{
+describe(@"changing the count", ^{
+    __block UIView *view;
+    __block RKNotificationHub *subject;
     
+    before(^{
+        view = [[UIView alloc] initWithFrame:CGRectZero];
+        subject = [[RKNotificationHub alloc] initWithView:nil];
+        [subject setView:view andCount:20];
+    });
+    
+    describe(@"increment", ^{
+        it(@"should increase count", ^{
+            [subject increment];
+            expect([subject count]).to.equal(21);
+        });
+    });
+    
+    describe(@"incrementBy:", ^{
+        it(@"should increase count by given value", ^{
+            [subject incrementBy:10];
+            expect([subject count]).to.equal(30);
+        });
+    });
+    
+    describe(@"decrement", ^{
+        it(@"should decrease count", ^{
+            [subject decrement];
+            expect([subject count]).to.equal(19);
+        });
+        
+        it(@"should not decrease count if count is 0", ^{
+            [subject setCount:0];
+            [subject decrement];
+            expect([subject count]).to.equal(0);
+        });
+    });
+    
+    describe(@"decrementBy:", ^{
+        it(@"should decrease count by given value", ^{
+            [subject decrementBy:15];
+            expect([subject count]).to.equal(5);
+        });
+        
+        it(@"should make count negative if given value is larger than the count", ^{
+            [subject decrementBy:40];
+            expect([subject count]).to.equal(-20);
+        });
+    });
+    
+    describe(@"setCount:", ^{
+        it(@"should update count with given value", ^{
+            [subject setCount:35];
+            expect([subject count]).to.equal(35);
+        });
+    });
 });
 
-describe(@"incrementBy:", ^{
+describe(@"animations", ^{
+    __block UIView *view;
+    __block RKNotificationHub *subject;
     
-});
-
-describe(@"decrement", ^{
+    before(^{
+        view = [[UIView alloc] initWithFrame:CGRectZero];
+        subject = [[RKNotificationHub alloc] initWithView:nil];
+        [subject setView:view andCount:20];
+    });
     
-});
-
-describe(@"decrementBy:", ^{
+    describe(@"pop", ^{
+        it(@"should not change count", ^{
+            [subject pop];
+            expect([subject count]).to.equal(20);
+        });
+    });
     
-});
-
-describe(@"setCount:", ^{
+    describe(@"blink", ^{
+        it(@"should not change count", ^{
+            [subject blink];
+            expect([subject count]).to.equal(20);
+        });
+    });
     
+    describe(@"bump", ^{
+        it(@"should not change count", ^{
+            [subject bump];
+            expect([subject count]).to.equal(20);
+        });
+    });
 });
-
-describe(@"count", ^{
-    
-});
-
-// hiding / showing the count
-
-describe(@"hideCount", ^{
-    
-});
-
-describe(@"showCount", ^{
-    
-});
-
-// animations
-
-describe(@"pop", ^{
-    
-});
-
-describe(@"blink", ^{
-    
-});
-
-describe(@"bump", ^{
-    
-});
-
-// snapshots
 
 describe(@"visuals", ^{
     __block UIView *backgroundView;
@@ -117,26 +154,28 @@ describe(@"visuals", ^{
         [subject setCount:5];
     });
 
-    it(@"should not display notification icon when count is 0", ^{
-        [subject setCount:0];
-        expect(backgroundView).to.haveValidSnapshot();
-    });
-    
-    it(@"should display notification icon when count is not zero", ^{
-        expect(backgroundView).to.haveValidSnapshotNamed(@"default_5");
-    });
-    
-    it(@"should have red background and white label by default", ^{
-        expect(backgroundView).to.haveValidSnapshotNamed(@"default_5");
-    });
-    
-    it(@"should display the icon centered in the top right corner of the target view by default", ^{
-        expect(backgroundView).to.haveValidSnapshotNamed(@"default_5");
-    });
-    
-    it(@"should adjust icon width for larger count", ^{
-        [subject setCount:375];
-        expect(backgroundView).to.haveValidSnapshot();
+    describe(@"appearance", ^{
+        it(@"should not display notification icon when count is 0", ^{
+            [subject setCount:0];
+            expect(backgroundView).to.haveValidSnapshot();
+        });
+        
+        it(@"should display notification icon when count is not zero", ^{
+            expect(backgroundView).to.haveValidSnapshotNamed(@"default_5");
+        });
+        
+        it(@"should have red background and white label by default", ^{
+            expect(backgroundView).to.haveValidSnapshotNamed(@"default_5");
+        });
+        
+        it(@"should display the icon centered in the top right corner of the target view by default", ^{
+            expect(backgroundView).to.haveValidSnapshotNamed(@"default_5");
+        });
+        
+        it(@"should adjust icon width for larger count", ^{
+            [subject setCount:375];
+            expect(backgroundView).to.haveValidSnapshot();
+        });
     });
     
     describe(@"adjustment methods", ^{
@@ -158,6 +197,44 @@ describe(@"visuals", ^{
         it(@"should look right with scaled notification icon", ^{
             [subject scaleCircleSizeBy:1.5];
             expect(backgroundView).to.haveValidSnapshot();
+        });
+    });
+    
+    describe(@"changing the count", ^{
+        it(@"should look right when count is incremented", ^{
+            [subject increment];
+            expect(backgroundView).to.haveValidSnapshot();
+        });
+        
+        it(@"should look right when count is incremented by value", ^{
+            [subject incrementBy:3];
+            expect(backgroundView).to.haveValidSnapshot();
+        });
+        
+        it(@"should look right when count is decremented", ^{
+            [subject decrement];
+            expect(backgroundView).to.haveValidSnapshot();
+        });
+        
+        it(@"should look right when count is decremented by value", ^{
+            [subject decrementBy:3];
+            expect(backgroundView).to.haveValidSnapshot();
+        });
+    });
+    
+    describe(@"hideCount", ^{
+        it(@"should look right with hidden count", ^{
+            [subject showCount];
+            [subject hideCount];
+            expect(backgroundView).to.haveValidSnapshot();
+        });
+    });
+    
+    describe(@"showCount", ^{
+        it(@"should look right with visible count", ^{
+            [subject hideCount];
+            [subject showCount];
+            expect(backgroundView).to.haveValidSnapshotNamed(@"default_5");
         });
     });
 });
